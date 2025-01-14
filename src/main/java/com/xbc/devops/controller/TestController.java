3a3e9fd492aac74a816b6d39f239b79b5769acec
Administrator <gitlab_admin_f44b09@example.com>
2025-01-15 01:54:01 +0800

    test sonar scanner 01

--- src/main/java/com/xbc/devops/controller/TestController.java
+++ src/main/java/com/xbc/devops/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.xbc.devops.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/devops")
 public class TestController {
+//    @Autowired
+
 
     @GetMapping("/test")
     public Object test(){
+        System.out.println("test");
         return "hello  test port 8866...........tag v1.6 ";
     }
 }
